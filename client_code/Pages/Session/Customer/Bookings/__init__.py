549721f2f9bee77d8bc495fbbd77b07a9fee5fdf
samuellepinay54@gmail.com <samuellepinay54@gmail.com>
2025-04-30 00:45:58 +0000

    Edited Pages.Session.Admin.ListClients.ListClientsCard, Pages.Session.Customer.Bookings, Pages.Session.Customer.Dashboard, Pages.Session.Customer.Profile and 1 more

--- client_code/Pages/Session/Customer/Bookings/__init__.py
+++ client_code/Pages/Session/Customer/Bookings/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import BookingsTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.server
 from anvil.tables import app_tables
 from ....NoSession.Products.Tables import Tables
--- server_code/Recu.py
+++ server_code/Recu.py
@@ -1,3 +1,6 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.server
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
